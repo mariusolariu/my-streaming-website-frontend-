fc573c34bc6914c2c09777b0d0e6fbaf8968fb1c
hafsacrypto <113556729+hafsacrypto@users.noreply.github.com>
2024-05-08 17:22:46 +0100

    implemented login page

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import './Login.css'; // Make sure to create this CSS file
+import React, { useState } from "react";
+import './Login.css'; 
+import Movies from "./Movies"
  
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [userCredentialsCorrect, setUsernameCredentialsCorrect] = useState(false);
+ 
+  const handleLogin = (event) => {
+    event.preventDefault(); // Prevent the form from being submitted to the server
+    if (username === 'admin' && password === '12345678') {
+        setUsernameCredentialsCorrect(true);
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  if (userCredentialsCorrect == true){
+    return <Movies/> 
+  }
+
   return (
     <div className="login-container">
       <div className="login-form">
         <h2>anime.world</h2>
-        <form>
+        <form onSubmit = {handleLogin}> 
           <div className="input-group">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input onChange = { (e) => setUsername(e.target.value) } type="text" id="username" />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input onChange = { (e) => setPassword(e.target.value) } type="password" id="password" />
           </div>
           
-          <button  className="input-group" classNamentype="submit">Login</button>
+          <button  id="myloginsubmitbutton" className="input-group" classNamentype="submit">Login</button>
         </form>
       </div>
     </div>
---
+++ src/Movies.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+function Movies() {
+    return <>here we will show the movies </>;
+
+ }
+
+ export default Movies;
