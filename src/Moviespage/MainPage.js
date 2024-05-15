384041e98a2334e794a5150f0fb88f547a18cb2e
hafsacrypto <113556729+hafsacrypto@users.noreply.github.com>
2024-05-15 19:03:47 +0100

    mainpage shows movie

--- src/Moviespage/MainPage.js
+++ src/Moviespage/MainPage.js
@@ -3,12 +3,12 @@ import MoviesRow from './MoviesRow';
 
 function MainPage() {
   const categories = [
-    { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/watch?v=zkYN0MI9nZI' }, /* More shows */] },
-    { title: 'News Live Up Next', shows: [{ id: 1, title: 'Anderson Cooper 360', url: 'https://www.youtube.com/watch?v=hbuZrg1ZAnE' }, /* More shows */] },
-    { title: 'Live TV', shows: [{ id: 2, title: 'The Last Ship', url: 'https://www.youtube.com/watch?v=ABG0BthfTOQ' }, /* More shows */] },
-    { title: 'Live TV', shows: [{ id: 3, title: 'The Last Ship', url: 'https://www.youtube.com/watch?v=sXWFr5Nk-g8' }, /* More shows */] },
-    { title: 'Live TV', shows: [{ id: 4, title: 'The Last Ship', url: 'https://www.youtube.com/watch?v=p6X6L4evKgs' }, /* More shows */] },
-    { title: 'Live TV', shows: [{ id: 5, title: 'The Last Ship', url: 'https://www.youtube.com/watch?v=MnKas07tYu8' }, /* More shows */] },
+    { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, /* More shows */] },
+    { title: 'News Live Up Next', shows: [{ id: 1, title: 'Anderson Cooper 360', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, /* More shows */] },
+    { title: 'Live TV', shows: [{ id: 2, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, /* More shows */] },
+    { title: 'Live TV', shows: [{ id: 3, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, /* More shows */] },
+    { title: 'Live TV', shows: [{ id: 4, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, /* More shows */] },
+    { title: 'Live TV', shows: [{ id: 5, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, /* More shows */] },
     // Add more categories as needed
   ];
  
--- src/Moviespage/MoviesCard.js
+++ src/Moviespage/MoviesCard.js
@@ -4,20 +4,7 @@ function MoviesCard({ id, shows }) {
   console.log(shows[0].url)  
   return (
     <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
-      <iframe
-        title={shows[0].title}
-        src={shows[0].url}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          border: 0
-        }}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
+      <iframe width="560" height="315" src={shows[0].url} title={shows[0].title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 </div>
   );
 }
