7af48df65ab4374f6c33410c0d7762974c0b35a2
hafsacrypto <113556729+hafsacrypto@users.noreply.github.com>
2024-05-17 20:10:46 +0100

    added new videos

--- src/Moviespage/MainPage.js
+++ src/Moviespage/MainPage.js
@@ -3,9 +3,9 @@ import MoviesRow from './MoviesRow';
 
 function MainPage() {
   const categories = [
-    { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, { id: 1, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, { id: 2, title: 'The Last Ship', url: 'https://www.youtube.com/embed/eVAd_S-7X-Y?si=KvvQZrhtcrf38gjS' }] }, 
+    { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, { id: 1, title: 'The Last Ship', url: 'https://www.youtube.com/embed/sXWFr5Nk-g8' }, { id: 2, title: 'The Last Ship', url: 'https://www.youtube.com/embed/pG2fSL1pC139mTWj'}] }, 
     { title: 'News Live Up Next', shows: [{ id: 0, title: 'Anderson Cooper 360', url: 'https://www.youtube.com/embed/zkYN0MI9nZI?si=rRFKJIkYOLZoq6X1' }, { id: 1, title: 'Anderson Cooper 360', url: 'https://www.youtube.com/embed/HVjQp54UboA?si=KapIbxwLJEaA4sQo' }, { id: 2, title: 'Anderson Cooper 360', url: 'https://www.youtube.com/embed/GIhh-HitJTk?si=tnxVCTdnN5LQ78dQ' }  ] },
-    { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/embed/GCJnG6DYEeg?si=FJ0eZj2r81j1Y4Ww' }, { id: 1, title: 'The Last Ship', url: ''} ] }, 
+    { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/embed/GCJnG6DYEeg?si=FJ0eZj2r81j1Y4Ww' }, { id: 1, title: 'The Last Ship', url: '""'} ] }, 
     { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/embed/E1_R7Ze84mo?si=jnOCeQL4jg95b9Kl"' }, ] },
     { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/embed/AiESdN5SC8s?si=Bdc7UF_0siSfA3ee' }, ] },
     { title: 'Live TV', shows: [{ id: 0, title: 'The Last Ship', url: 'https://www.youtube.com/embed/5h0L68KEdxc?si=pMzXt_FZJdyngpa3' }, ] },
@@ -13,7 +13,7 @@ function MainPage() {
   ];
  
   return (
-    <div style={{ backgroundColor: 'black', color: 'white', padding: '20px' }}>
+    <div style={{ backgroundColor: 'black', color: 'white'}}>
       {categories.map(category => (
         <MoviesRow id={category.shows[0].id} key={category.shows[0].id} title={category.title} shows={category.shows} />
       ))}
--- src/Moviespage/MoviesCard.js
+++ src/Moviespage/MoviesCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
  
 function MoviesCard({ id, shows }) {
-  console.log(shows[0].url)  
+  console.log(shows[id].url)  
   return (
     <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, paddingRight: "15px" }}>
       <iframe width="200" height="150" src={shows[id].url} title={shows[id].title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
